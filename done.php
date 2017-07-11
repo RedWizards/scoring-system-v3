5ef9b144decab98c66f2c44e14e01c992e5bdd6b
redperiabras <redperiabras@gmail.com>
2017-07-12 02:04:02 +0800

    done page

---
+++ done.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+
+	<head>
+
+		<title>UHAC Cebu</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="assets/images/uhac.ico" type="image/ico" sizes="32x32">
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<!--
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		-->
+		<link rel="stylesheet" href="assets/css/judge-view.css">
+
+	</head>
+
+	<body>
+
+			<table id="this-cover">
+				<tr>
+					<td>
+
+						<div id="p1" class="text-center sub">
+							<h3><b>UHAC</b> Cebu</h3>
+							<small>SCORING SYSTEM</small>
+						</div>
+									
+						<div id="thanks" class="text-center">
+							<h1 id="greet">THANK YOU FOR JUDGING</h1>
+						</div>
+									
+						<div id="p2" class="text-center sub">
+							<small>POWERED BY</small><br/>
+							<span><strong>RED Wizard Events Management</strong></span><br/>
+							<small><i>Created by </i></small>
+							<span>Mamba Codes</span>
+						</div>
+					</td>
+				</tr>
+			</table>
+
+	</body>
+
+	<script src="assets/js/jquery.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/angular.min.js"></script>
+	<script src="assets/js/angular-animate.js"></script>
+	<script src="assets/js/judge-view.js"></script>
+	<script src="assets/js/angular-route.js"></script>
+
+</html>

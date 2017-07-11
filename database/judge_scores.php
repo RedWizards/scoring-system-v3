f920983317a5588a8e30b3ec23f6870b6fd54021
redperiabras <redperiabras@gmail.com>
2017-07-11 14:33:33 +0800

    removing other judges routes

--- database/judge_scores.php
+++ database/judge_scores.php
@@ -1,4 +1,3 @@
-
 <?php
 	header('Content-type: application/json');
 	require_once('connection.php');
--- helpers/logout.php
+++ helpers/logout.php
@@ -1,4 +1,5 @@
 <?php
 	session_start();
 	unset($_SESSION['judge_id']);
+	//redirect to done page
 ?>

45ba0999e07454ebbca8908dbd4d6ad4198320d5
redperiabras <redperiabras@gmail.com>
2017-07-11 21:45:52 +0800

    Landing with registration

---
+++ assets/js/judge-view.js
@@ -0,0 +1,19 @@
+function regJudgeOpen() {
+	document.getElementById("event-cover").style.height = 0;
+	document.getElementById("event-cover").style.opacity = 0;
+}
+
+function regJudgeClose() {
+	document.getElementById("event-cover").style.height = "100%";
+	document.getElementById("event-cover").style.opacity = 1;
+}
+
+function submitName(){
+	if($('#input-container').val() == ""){
+
+	} else {
+		$(this).button('loading').queue(function() {
+			            
+		}); 
+	}       
+}

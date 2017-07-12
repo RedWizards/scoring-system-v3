39c1a1ac573fbc048affadc5639f3efdac0aa371
redperiabras <redperiabras@gmail.com>
2017-07-12 13:54:07 +0800

    asdf

--- assets/js/judge-scoresheet.js
+++ assets/js/judge-scoresheet.js
@@ -20,6 +20,7 @@
 					}
 				})
 				.done(function(data){
+					console.log(data);
 					$scope.teams = data;
 					$scope.$apply();
 				})
@@ -30,20 +31,46 @@
 		}
 
 		$scope.setScores = function(team){
-			var sheet_url= '../../database/submit_score.php';
 			
 			var success = true;
 
 			for(var i = 0; i < team.criteria.length; i++){
-				$.ajax({
-					url: sheet_url,
-					data:{
-						score_id: team.criteria[i].score_details.score_id,
-						score: team.criteria[i].score_details.score
-					}
-				}).error(function(){
-					success = false;
-				});
+
+				if((team.criteria[i].score_details.score_id == null) || (team.criteria[i].score_details.score_id == 0)){
+					//add scores
+					$.ajax({
+						method: 'post',
+						url: '../../database/submit_score.php',
+						data:{
+							judge_id: $scope.session.judge_id,
+							criteria_id: team.criteria[i].id,
+							project_id: team.project_id,
+							score: team.criteria[i].score_details.score
+						}
+					})
+					.done(function(data){
+						team.criteria[i].score_details.score_id = data.id;
+					})
+					.fail(function(xhr, textStatus, errorThrown) {
+					    console.log(xhr.responseText);
+					    success= false;
+					});
+				}
+				else{
+					//update scores
+					$.ajax({
+						method: 'post',
+						url: '../../database/update_scores.php',
+						data:{
+							score_id: team.criteria[i].score_details.score_id,
+							score: team.criteria[i].score_details.score
+						}
+					}).fail(function(xhr, textStatus, errorThrown) {
+					    console.log(xhr.responseText);
+					    success= false;
+					});
+				}
+				
 			}
 
 			if(success == true){

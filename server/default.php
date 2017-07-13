3e8458b0f2de9ff8406d15e0e1bbbea3e59ff88e
Prince Julius Hari <princejulius230@gmail.com>
2017-07-14 02:01:58 +0800

    Admin Security UI

---
+++ server/default.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+
+	<head>
+
+		<title>UHAC Cebu</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="../assets/images/uhac.ico" type="image/ico" sizes="32x32">
+		<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../assets/css/admin-view.css">
+
+	</head>
+
+	<body>
+
+		<div id="register-judge">
+			<table>
+				<tr>
+					<td>
+						<div id="p4" class="text-center">
+							<h4 class="sub">Authorized Personnel Only</h4>
+						</div>
+
+						<div id="p3" class="text-center">
+							<form class="form">
+			        			<input placeholder="PASSWORD" type="password" id="input-container" class="form-control text-center" autofocus required><br/>
+								<input id="register-button" class="btn" class="form-control" data-loading-text="Please Wait..." type="submit" value="ENTER">
+			        		</form>
+						</div>
+					</td>
+				</tr>
+			</table> 
+		</div>
+
+	</body>
+
+	<script src="../assets/js/jquery.js"></script>
+	<script src="../assets/js/bootstrap.min.js"></script>
+	<script src="../assets/js/angular.min.js"></script>
+
+</html>
